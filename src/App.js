1f585e948213d5c82253b8c2d87a7e7458e1e0b1
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-10-09 23:25:55 +0300

    finished part2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Profile from './Components/Profile';
+import Statistic from './Components/Statistic';
 import user from './data/user.json';
+import statisticalData from './data/statistical-data.json';
 import './App.css';
 
 function App() {
@@ -13,6 +15,10 @@ function App() {
           avatar={user.avatar}
           stats={user.stats}
         />
+      </div>
+      <div className="partTwo">
+        <Statistic title="Upload Stats" stats={statisticalData} />
+        <Statistic stats={statisticalData} />
       </div>
     </div>
   );
