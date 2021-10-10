d1c3ddcad9f671ff2c7ae1e312c9bb21626ada14
RomanVoznyy <roman.voznyy.52@gmail.com>
2021-10-10 22:51:15 +0300

    finished part 3 and 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import Profile from './Components/Profile';
 import Statistic from './Components/Statistic';
+import FriendList from './Components/FriendList/FriendList';
+import TransactionHistory from './Components/TransactionHistory';
 import user from './data/user.json';
 import statisticalData from './data/statistical-data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
 import './App.css';
 
 function App() {
@@ -19,6 +23,12 @@ function App() {
       <div className="partTwo">
         <Statistic title="Upload Stats" stats={statisticalData} />
         <Statistic stats={statisticalData} />
+      </div>
+      <div className="partThree">
+        <FriendList friends={friends} />
+      </div>
+      <div className="partFour">
+        <TransactionHistory items={transactions} />
       </div>
     </div>
   );
